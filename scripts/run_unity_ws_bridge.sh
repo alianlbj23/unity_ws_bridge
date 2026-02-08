851facab9006f9cf34ae4a0d4e48420fda5d070a
alianlbj23 <alianlbj23@gmail.com>
2026-02-08 10:05:52 +0800

    change port

--- scripts/run_unity_ws_bridge.sh
+++ scripts/run_unity_ws_bridge.sh
@@ -15,6 +15,6 @@ source "${WS_DIR}/install/setup.bash"
 
 ros2 run unity_ws_bridge unity_ws_bridge_node --ros-args \
   -p bind_address:=0.0.0.0 \
-  -p port:=8767 \
+  -p port:=8765 \
   -p scan_topic:=/scan \
   -p default_frame_id:=ms200k
